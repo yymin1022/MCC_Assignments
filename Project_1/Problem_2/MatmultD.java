4f3b6b8e4feb9fa869e591dede6b44f3cf2b33a9
yymin1022 <yymin1022@gmail.com>
2024-04-17 01:41:47 +0900

    Update Assignment 1 : MatmultD Cleaned UP

--- Project_1/Problem_2/MatmultD.java
+++ Project_1/Problem_2/MatmultD.java
@@ -8,76 +8,77 @@ import java.lang.*;
 // In eclipse, set the argument value and file input by using the menu [Run]->[Run Configurations]->{[Arguments], [Common->Input File]}.
 
 // Original JAVA source code: http://stackoverflow.com/questions/21547462/how-to-multiply-2-dimensional-arrays-matrix-multiplication
-public class MatmultD
-{
-  private static Scanner sc = new Scanner(System.in);
-  public static void main(String [] args)
-  {
-    int thread_no=0;
-    if (args.length==1) thread_no = Integer.valueOf(args[0]);
-    else thread_no = 1;
-        
-    int a[][]=readMatrix();
-    int b[][]=readMatrix();
+public class MatmultD {
+    private static final Scanner sc = new Scanner(System.in);
 
-    long startTime = System.currentTimeMillis();
-    int[][] c=multMatrix(a,b);
-    long endTime = System.currentTimeMillis();
+    public static void main(String[] args) {
+        int thread_no = args.length == 1 ? Integer.parseInt(args[0]) : 1;
+        int[][] matrixA = readMatrix();
+        int[][] matrixB = readMatrix();
 
-    //printMatrix(a);
-    //printMatrix(b);    
-    printMatrix(c);
+        long startTime = System.currentTimeMillis();
+        int[][] matrixC = multMatrix(matrixA, matrixB);
+        long endTime = System.currentTimeMillis();
 
-    //System.out.printf("thread_no: %d\n" , thread_no);
-    //System.out.printf("Calculation Time: %d ms\n" , endTime-startTime);
+        printMatrix(matrixC);
 
-    System.out.printf("[thread_no]:%2d , [Time]:%4d ms\n", thread_no, endTime-startTime);
-  }
+        //System.out.printf("thread_no: %d\n" , thread_no);
+        //System.out.printf("Calculation Time: %d ms\n" , endTime-startTime);
 
-   public static int[][] readMatrix() {
-       int rows = sc.nextInt();
-       int cols = sc.nextInt();
-       int[][] result = new int[rows][cols];
-       for (int i = 0; i < rows; i++) {
-           for (int j = 0; j < cols; j++) {
-              result[i][j] = sc.nextInt();
-           }
-       }
-       return result;
-   }
-
-  public static void printMatrix(int[][] mat) {
-  System.out.println("Matrix["+mat.length+"]["+mat[0].length+"]");
-    int rows = mat.length;
-    int columns = mat[0].length;
-    int sum = 0;
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < columns; j++) {
-        System.out.printf("%4d " , mat[i][j]);
-        sum+=mat[i][j];
-      }
-      System.out.println();
+        System.out.printf("[thread_no]:%2d , [Time]:%4d ms\n", thread_no, endTime-startTime);
     }
-    System.out.println();
-    System.out.println("Matrix Sum = " + sum + "\n");
-  }
 
-  public static int[][] multMatrix(int a[][], int b[][]){//a[m][n], b[n][p]
-    if(a.length == 0) return new int[0][0];
-    if(a[0].length != b.length) return null; //invalid dims
+    public static int[][] readMatrix() {
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] result = new int[rows][cols];
 
-    int n = a[0].length;
-    int m = a.length;
-    int p = b[0].length;
-    int ans[][] = new int[m][p];
-
-    for(int i = 0;i < m;i++){
-      for(int j = 0;j < p;j++){
-        for(int k = 0;k < n;k++){
-          ans[i][j] += a[i][k] * b[k][j];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = sc.nextInt();
+            }
         }
-      }
+
+        return result;
     }
-    return ans;
-  }
+
+    public static void printMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        System.out.printf("Matrix[%d][%d]\n", rows, cols);
+
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum += matrix[i][j];
+                System.out.printf("%4d ", matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.printf("Matrix Sum = %d\n", sum);
+    }
+
+    public static int[][] multMatrix(int[][] matrixA, int[][] matrixB){
+        if (matrixA.length == 0) {
+            return new int[0][0];
+        }
+
+        if (matrixA[0].length != matrixB.length) {
+            return null;
+        }
+
+        int rows = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
+        int[][] ans = new int[colsA][colsB];
+        for (int i = 0; i < rows; i++){
+            for(int j = 0; j < colsB; j++){
+                for(int k = 0; k < colsA; k++){
+                    ans[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+        return ans;
+    }
 }
