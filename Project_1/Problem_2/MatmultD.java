95fa587796891fde6f016d21beaf9b1b8662fc77
yymin1022 <yymin1022@gmail.com>
2024-04-17 03:15:04 +0900

    Update Assignment 1 : Applied Multi-Threading for MatmultD

--- Project_1/Problem_2/MatmultD.java
+++ Project_1/Problem_2/MatmultD.java
@@ -11,21 +11,43 @@ import java.lang.*;
 public class MatmultD {
     private static final Scanner sc = new Scanner(System.in);
 
+    static int NUM_THREADS;
+    static int[][] MATRIX_A;
+    static int[][] MATRIX_B;
+
     public static void main(String[] args) {
-        int thread_no = args.length == 1 ? Integer.parseInt(args[0]) : 1;
-        int[][] matrixA = readMatrix();
-        int[][] matrixB = readMatrix();
+        NUM_THREADS = args.length == 1 ? Integer.parseInt(args[0]) : 1;
+        MATRIX_A = readMatrix();
+        MATRIX_B = readMatrix();
 
         long startTime = System.currentTimeMillis();
-        int[][] matrixC = multMatrix(matrixA, matrixB);
+
+        MatmultD.MultiplyMatrixThread[] threads = new MatmultD.MultiplyMatrixThread[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i] = new MatmultD.MultiplyMatrixThread(i, NUM_THREADS);
+        }
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i].start();
+        }
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.err.println("Error in thread " + e);
+            }
+        }
+
         long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
 
-        printMatrix(matrixC);
+        for (int i = 0; i < NUM_THREADS; i++) {
+            System.out.printf("Thread #%d Execution Time: %dms\n", i, threads[i].timeDiff);
+        }
 
-        //System.out.printf("thread_no: %d\n" , thread_no);
-        //System.out.printf("Calculation Time: %d ms\n" , endTime-startTime);
-
-        System.out.printf("[thread_no]:%2d , [Time]:%4d ms\n", thread_no, endTime-startTime);
+        System.out.println("Program Execution Time: " + timeDiff + "ms");
+        printMatrix(MultiplyMatrixThread.matrixResult);
     }
 
     public static int[][] readMatrix() {
@@ -58,27 +80,50 @@ public class MatmultD {
         System.out.printf("Matrix Sum = %d\n", sum);
     }
 
-    public static int[][] multMatrix(int[][] matrixA, int[][] matrixB){
-        if (matrixA.length == 0) {
-            return new int[0][0];
+    static class MultiplyMatrixThread extends Thread {
+        static int[][] matrixResult;
+        static int matrixSum;
+        static int cur_row;
+
+        int thread_cnt;
+        int thread_idx;
+        int tmpCount;
+        long timeDiff;
+
+        public MultiplyMatrixThread(int idx, int cnt) {
+            this.thread_cnt = cnt;
+            this.thread_idx = idx;
+
+            matrixResult = new int[MATRIX_A.length][MATRIX_B[0].length];
+            matrixSum = 0;
         }
 
-        if (matrixA[0].length != matrixB.length) {
-            return null;
-        }
+        @Override
+        public void run() {
+            tmpCount = 0;
 
-        int rows = matrixA.length;
-        int colsA = matrixA[0].length;
-        int colsB = matrixB[0].length;
-        int[][] ans = new int[colsA][colsB];
-        for (int i = 0; i < rows; i++){
-            for(int j = 0; j < colsB; j++){
-                for(int k = 0; k < colsA; k++){
-                    ans[i][j] += matrixA[i][k] * matrixB[k][j];
-                }
+            int numFrom = (MATRIX_A.length / thread_cnt) * thread_idx;
+            int numTo = (thread_idx + 1 != thread_cnt ? (MATRIX_A.length / thread_cnt) * (thread_idx + 1) : MATRIX_A.length);
+
+            long startTime = System.currentTimeMillis();
+
+            while (cur_row < numFrom) {
+                Thread.yield();
             }
-        }
 
-        return ans;
+            System.out.println("Thread #" + thread_idx + " Row from " + numFrom + " to " + numTo);
+            while (cur_row < numTo) {
+                for(int i = 0; i < MATRIX_B[0].length; i++){
+                    for(int j = 0; j < MATRIX_A[0].length; j++){
+                        matrixResult[cur_row][i] += MATRIX_A[cur_row][j] * MATRIX_B[j][i];
+                    }
+                }
+
+                cur_row++;
+            }
+
+            long endTime = System.currentTimeMillis();
+            timeDiff = endTime - startTime;
+        }
     }
 }
