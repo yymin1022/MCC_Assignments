a062748d2890206181214701bd361816c1410229
yymin1022 <yymin1022@gmail.com>
2024-04-16 23:45:56 +0900

    Update Assignment 1 : Cyclic Block Thread

--- Project_1/pc_static_cycle.java
+++ Project_1/pc_static_cycle.java
@@ -1,27 +1,43 @@
 public class pc_static_cycle {
     private static int NUM_END = 200000;
-    private static int NUM_THREADS = 1;
+    private static int NUM_THREADS = 4;
 
     public static void main(String[] args) {
-        if (args.length==2) {
+        if (args.length == 2) {
             NUM_THREADS = Integer.parseInt(args[0]);
             NUM_END = Integer.parseInt(args[1]);
         }
 
-        int counter = 0;
+        System.out.println("Static Cyclic Multi-Thread with " + NUM_THREADS + " threads.");
+
         long startTime = System.currentTimeMillis();
-        int i;
-        for (i = 0; i < NUM_END; i++) {
-            if (isPrime(i)) {
-                counter++;
+
+        pc_static_cycle.StaticCyclicThread[] threads = new pc_static_cycle.StaticCyclicThread[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i] = new pc_static_cycle.StaticCyclicThread(i, NUM_THREADS);
+        }
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i].start();
+        }
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.err.println("Error in thread " + e);
             }
         }
 
         long endTime = System.currentTimeMillis();
         long timeDiff = endTime - startTime;
 
+        for (int i = 0; i < NUM_THREADS; i++) {
+            System.out.printf("Thread #%d Execution Time: %dms\n", i, threads[i].timeDiff);
+        }
+
         System.out.println("Program Execution Time: " + timeDiff + "ms");
-        System.out.println("1..." + (NUM_END - 1) + " prime# counter=" + counter);
+        System.out.println("1..." + (NUM_END - 1) + " prime# counter=" + pc_static_cycle.StaticCyclicThread.count);
     }
 
     private static boolean isPrime(int x) {
@@ -36,5 +52,47 @@ public class pc_static_cycle {
             }
         }
         return true;
+    }
+
+    static class StaticCyclicThread extends Thread {
+        static int count;
+        static int num;
+
+        int thread_cnt;
+        int thread_idx;
+        int tmpCount;
+        long timeDiff;
+
+        public StaticCyclicThread(int idx, int cnt) {
+            this.thread_cnt = cnt;
+            this.thread_idx = idx;
+        }
+
+        @Override
+        public void run() {
+            tmpCount = 0;
+
+            while (num < thread_idx * 10) {
+                Thread.yield();
+            }
+
+            long startTime = System.currentTimeMillis();
+
+            while (num < NUM_END) {
+                if ((num / 10 * 10 - thread_idx * 10) % (thread_cnt * 10) != 0) {
+                    Thread.yield();
+                    continue;
+                }
+
+                if (isPrime(num)) {
+                    count++;
+                }
+
+                num++;
+            }
+
+            long endTime = System.currentTimeMillis();
+            timeDiff = endTime - startTime;
+        }
     }
 }
