e1a1ec34d01eb9080b79c603baf7e5558d85d12d
yymin1022 <yymin1022@gmail.com>
2024-05-10 05:20:15 +0900

    Update Assignment 2 : Added ex4 CyclicBarrier for Problem 3

--- Project_2/prob3/ex4.java
+++ Project_2/prob3/ex4.java
@@ -1,5 +1,50 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class ex4 {
+    private final CyclicBarrier barrier = new CyclicBarrier(2, () -> {});
+    int num = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ex4 example = new ex4();
+        Thread producerThread = new Thread(() -> {
+            try {
+                example.producer();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (BrokenBarrierException e) {
+                System.out.println("Barrier was broken!");
+            }
+        }, "Producer Thread");
+
+        Thread consumerThread = new Thread(() -> {
+            try {
+                example.consumer();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (BrokenBarrierException e) {
+                System.out.println("Barrier was broken!");
+            }
+        }, "Consumer Thread");
+
+        producerThread.start();
+        consumerThread.start();
+    }
+
+    public void producer() throws InterruptedException, BrokenBarrierException {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Producing " + num);
+            num++;
+            Thread.sleep(1000);
+        }
+        barrier.await();
+    }
+
+    public void consumer() throws InterruptedException, BrokenBarrierException {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Consuming " + num);
+            Thread.sleep(1000);
+        }
+        barrier.await();
     }
 }
