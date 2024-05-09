b6adc559c6001071f691a6f897f3fcc7b98c8eac
yymin1022 <yymin1022@gmail.com>
2024-05-10 02:47:34 +0900

    Update Assignment 2 : Cleaned Problem 2

--- Project_2/prob2/ParkingSemaphore.java
+++ Project_2/prob2/ParkingSemaphore.java
@@ -1,86 +1,89 @@
 
 class ParkingGarageS {
-  private int places;
-  public ParkingGarageS(int places) {
-    if (places < 0)
-      places = 0;
-    this.places = places;
-  }
-  public synchronized void enter() { // enter parking garage
-    while (places == 0) {
-      try {
-        wait();
-      } catch (InterruptedException e) {}
+    private int places;
+
+    public ParkingGarageS(int places) {
+        if(places < 0){
+            places = 0;
+        }
+
+        this.places = places;
     }
-    places--;
-  }
-  public synchronized void leave() { // leave parking garage
-    places++;
-    notify();
-  }
-  public synchronized int getPlaces()
-  {
-    return places;
-  }
+
+    public synchronized void enter() {
+        while (places == 0) {
+            try {
+                wait();
+            } catch(InterruptedException e) {}
+        }
+        places--;
+    }
+
+    public synchronized void leave() {
+        places++;
+        notify();
+    }
+
+    public synchronized int getPlaces() {
+        return places;
+    }
 }
 
 
 class CarS extends Thread {
-  private ParkingGarageS parkingGarage;
-  public CarS(String name, ParkingGarageS p) {
-    super(name);
-    this.parkingGarage = p;
-    start();
-  }
+    private ParkingGarageS parkingGarage;
 
-  private void tryingEnter()
-  {
-      System.out.println(getName()+": trying to enter");
-  }
-
-
-  private void justEntered()
-  {
-      System.out.println(getName()+": just entered");
-
-  }
-
-  private void aboutToLeave()
-  {
-      System.out.println(getName()+":                                     about to leave");
-  }
-
-  private void Left()
-  {
-      System.out.println(getName()+":                                     have been left");
-  }
-
-  public void run() {
-    while (true) {
-      try {
-        sleep((int)(Math.random() * 10000)); // drive before parking
-      } catch (InterruptedException e) {}
-      tryingEnter();
-      parkingGarage.enter();
-      justEntered();
-      try {
-        sleep((int)(Math.random() * 20000)); // stay within the parking garage
-      } catch (InterruptedException e) {}
-      aboutToLeave();
-      parkingGarage.leave();
-      Left();
-
+    public CarS(String name, ParkingGarageS p) {
+        super(name);
+        this.parkingGarage = p;
+        start();
     }
-  }
+
+    private void tryingEnter() {
+        System.out.println(getName()+": trying to enter");
+    }
+
+
+    private void justEntered() {
+        System.out.println(getName()+": just entered");
+    }
+
+    private void aboutToLeave() {
+        System.out.println(getName()+":                                     about to leave");
+    }
+
+    private void Left() {
+        System.out.println(getName()+":                                     have been left");
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                sleep((int)(Math.random() * 10000));
+            } catch(InterruptedException e) {}
+
+            tryingEnter();
+            parkingGarage.enter();
+            justEntered();
+
+            try {
+                sleep((int)(Math.random() * 20000));
+            } catch(InterruptedException e) {}
+
+            aboutToLeave();
+            parkingGarage.leave();
+            Left();
+        }
+    }
 }
 
 
 public class ParkingSemaphore {
-  public static void main(String[] args){
-    ParkingGarageS parkingGarage = new ParkingGarageS(7);
-    for (int i=1; i<= 10; i++) {
-      CarS c = new CarS("Car "+i, parkingGarage);
+    public static void main(String[] args) {
+        ParkingGarageS parkingGarage = new ParkingGarageS(7);
+        for(int i = 1; i <= 10; i++) {
+            CarS c = new CarS("Car " + i, parkingGarage);
+        }
     }
-  }
 }
 
