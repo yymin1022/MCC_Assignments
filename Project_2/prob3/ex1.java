cf936a0be030bd7b0e873f7f1d877a43bb984533
yymin1022 <yymin1022@gmail.com>
2024-05-10 04:56:31 +0900

    Update Assignment 2 : Added ex1 ArrayBlockingQueue for Problem 3

--- Project_2/prob3/ex1.java
+++ Project_2/prob3/ex1.java
@@ -1,5 +1,44 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class ex1 {
+    private final ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ex1 example = new ex1();
+        Thread producerThread = new Thread(() -> {
+            try {
+                example.producer();
+            } catch(InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "Producer Thread");
+
+        Thread consumerThread = new Thread(() -> {
+            try {
+                example.consumer();
+            } catch(InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "Consumer Thread");
+
+        producerThread.start();
+        consumerThread.start();
+    }
+
+    public void producer() throws InterruptedException {
+        int value = 0;
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Producing " + value);
+            queue.put(value++);
+            Thread.sleep(1000);
+        }
+    }
+
+    public void consumer() throws InterruptedException {
+        for(int i = 0; i < 10; i++) {
+            int value = queue.take();
+            System.out.println("Consuming " + value);
+            Thread.sleep(1000);
+        }
     }
 }
