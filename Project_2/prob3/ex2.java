6141138a0328506ace8164c64d9a7502514ef657
yymin1022 <yymin1022@gmail.com>
2024-05-10 05:04:14 +0900

    Update Assignment 2 : Added ex2 ReadWriteLock for Problem 3

--- Project_2/prob3/ex2.java
+++ Project_2/prob3/ex2.java
@@ -1,5 +1,58 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class ex2 {
+    private final List<Integer> list = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ex2 example = new ex2();
+        Thread producerThread = new Thread(() -> {
+            try {
+                example.producer();
+            } catch(InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "Producer Thread");
+
+        Thread consumerThread = new Thread(() -> {
+            try {
+                example.consumer();
+            } catch(InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "Consumer Thread");
+
+        producerThread.start();
+        consumerThread.start();
+    }
+
+    public void producer() throws InterruptedException {
+        int value = 0;
+        for(int i = 0; i < 10; i++) {
+            lock.writeLock().lock();
+
+            System.out.println("Producing " + value);
+            list.add(value++);
+
+            lock.writeLock().unlock();
+            Thread.sleep(1000);
+        }
+    }
+
+    public void consumer() throws InterruptedException {
+        for(int i = 0; i < 10; i++) {
+            lock.readLock().lock();
+
+            if(!list.isEmpty()) {
+                int value = list.remove(0);
+                System.out.println("Consuming " + value);
+            }
+
+            lock.readLock().unlock();
+            Thread.sleep(1000);
+        }
     }
 }
