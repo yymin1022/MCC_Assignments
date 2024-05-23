2ea5d508b37d3136dd7ae8467f709a8a3dbf2eff
yymin1022 <yymin1022@gmail.com>
2024-05-23 17:32:13 +0900

    Update Assignment 3 : Added Argument Reading for Prob 1

--- Project_3/prob1/prob1.c
+++ Project_3/prob1/prob1.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
+int     parse_schedule_type(char *str);
+int     parse_thread_cnt(char *str);
+void    error_exit(char *msg);
+
+int main(int argc, char **argv)
 {
+    int schedule_type;
+    int thread_cnt;
+
+    if (argc != 3)
+        error_exit("Invalid Argument! Usage: ./prob1 scheduling_type# #_of_thread");
+    schedule_type = parse_schedule_type(argv[1]);
+    thread_cnt = parse_thread_cnt(argv[2]);
+    if (schedule_type == -1 || thread_cnt == -1)
+        error_exit("Invalid Argument Value!");
     printf("Hello, World!\n");
 
     return (0);
+}
+
+int parse_schedule_type(char *str)
+{
+    int res;
+
+    res = strtol(str, NULL, 10);
+    if (res < 1 || res > 4)
+        return (-1);
+    return (res);
+}
+
+int parse_thread_cnt(char *str)
+{
+    int res;
+
+    res = strtol(str, NULL, 10);
+    if ((res > 1 && res % 2 == 1) || res < 1 || res > 16)
+        return (-1);
+    return (res);
+}
+
+void    error_exit(char *msg)
+{
+    dprintf(2, "Error: %s\n", msg);
+    exit(1);
 }
