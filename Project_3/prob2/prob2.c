97f8be842a2c5ea91936d5220610dd298533c7d1
yymin1022 <yymin1022@gmail.com>
2024-05-25 04:26:03 +0900

    Update Assignment 3 : Cleaned UP for Prob 2

--- Project_3/prob2/prob2.c
+++ Project_3/prob2/prob2.c
@@ -1,16 +1,20 @@
-#include </opt/homebrew/Cellar/libomp/18.1.6/include/omp.h>
 #include <stdio.h>
+#include </opt/homebrew/Cellar/libomp/18.1.6/include/omp.h>
 
 double  step;
 long    num_steps = 10000000;
 
 int main(void)
 {
-    double x, pi;
-    double start_time, end_time;
-    double sum = 0.0;
+    double  x;
+    double  pi;
+    double  sum;
+    double  time_diff;
+    double  time_end;
+    double  time_start;
 
-    start_time = omp_get_wtime();
+    time_start = omp_get_wtime();
+
     step = 1.0 / (double)num_steps;
     for (long i = 0; i < num_steps; i++)
     {
@@ -18,10 +22,12 @@ int main(void)
         sum = sum + 4.0 / (1.0 + x * x);
     }
     pi = step * sum;
-    end_time = omp_get_wtime();
-    double timeDiff = end_time - start_time;
-    printf("Execution Time : %lfs\n", timeDiff);
-    printf("pi=%.24lf\n",pi);
+
+    time_end = omp_get_wtime();
+    time_diff = time_end - time_start;
+
+    printf("Execution Time : %lfs\n", time_diff);
+    printf("pi=%.24lf\n", pi);
 
     return (0);
 }
