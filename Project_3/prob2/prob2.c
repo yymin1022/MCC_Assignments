b0c9eef49abca2a82392fa93823b4088655a883c
yymin1022 <yymin1022@gmail.com>
2024-05-25 04:29:38 +0900

    Update Assignment 3 : Added Argument Parsing for Prob 2

--- Project_3/prob2/prob2.c
+++ Project_3/prob2/prob2.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include </opt/homebrew/Cellar/libomp/18.1.6/include/omp.h>
 
+#define NUM_STEPS 10000000
+
 double  step;
-long    num_steps = 10000000;
+int     parse_chunk_size(char *str);
+int     parse_schedule_type(char *str);
+int     parse_thread_cnt(char *str);
+void    error_exit(char *msg);
 
 int main(void)
 {
@@ -12,11 +17,23 @@ int main(void)
     double  time_diff;
     double  time_end;
     double  time_start;
+    int     chunk_size;
+    int     schedule_type;
+    int     thread_cnt;
+
+    if (argc != 4)
+        error_exit("Invalid Argument! Usage: ./prob1 scheduling_type# #_of_thread");
+
+    chunk_size = parse_chunk_size(argv[2]);
+    schedule_type = parse_schedule_type(argv[1]);
+    thread_cnt = parse_thread_cnt(argv[3]);
+    if (chunk_size == -1 || schedule_type == -1 || thread_cnt == -1)
+        error_exit("Invalid Argument Value!");
 
     time_start = omp_get_wtime();
 
     step = 1.0 / (double)num_steps;
-    for (long i = 0; i < num_steps; i++)
+    for (long i = 0; i < NUM_STEPS; i++)
     {
         x = (i + 0.5) * step;
         sum = sum + 4.0 / (1.0 + x * x);
@@ -30,4 +47,40 @@ int main(void)
     printf("pi=%.24lf\n", pi);
 
     return (0);
+}
+
+int parse_chunk_size(char *str)
+{
+    int res;
+
+    res = strtol(str, NULL, 10);
+    if (res < 1)
+        return (-1);
+    return (res);
+}
+
+int parse_schedule_type(char *str)
+{
+    int res;
+
+    res = strtol(str, NULL, 10);
+    if (res < 1 || res > 4)
+        return (-1);
+    return (res);
+}
+
+int parse_thread_cnt(char *str)
+{
+    int res;
+
+    res = strtol(str, NULL, 10);
+    if ((res > 1 && res % 2 == 1) || res < 1 || res > 16)
+        return (-1);
+    return (res);
+}
+
+void    error_exit(char *msg)
+{
+    dprintf(2, "Error: %s\n", msg);
+    exit(1);
 }
